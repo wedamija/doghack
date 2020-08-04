f82e54008a340faac516dfa235f56d72a8951c5d
Dan Fuller <dfuller@sentry.io>
2020-08-03 22:15:24 -0700

    Finished 1.3, random map generation working

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod map;
 pub use map::*;
 mod player;
 pub use player::*;
+mod rect;
+pub use rect::Rect;
 
 pub struct State {
     ecs: World,
@@ -42,9 +44,16 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
+
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
@@ -52,6 +61,5 @@ fn main() {
         })
         .with(Player {})
         .build();
-    gs.ecs.insert(new_map_test());
     rltk::main_loop(context, gs);
 }
